966706d3b45820f57fcd0bcc24aa9c7b7ac06643
강유욱[오디오클립] <yw.kang@navercorp.com>
2018-01-07 20:13:53 +0900

    example2

--- src/main/java/Send2.java
+++ src/main/java/Send2.java
@@ -2,7 +2,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-public class Send {
+public class Send2 {
 	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] argv) throws Exception {
@@ -12,12 +12,27 @@ public class Send {
 		Channel channel = connection.createChannel();
 
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		String message = "Hello World!";
-
+		String message = getMessage(argv);
 		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
 		System.out.println(" [x] Sent '" + message + "'");
 
 		channel.close();
 		connection.close();
+	}
+
+	private static String getMessage(String[] strings){
+		if (strings.length < 1)
+			return "Hello World!";
+		return joinStrings(strings, " ");
+	}
+
+	private static String joinStrings(String[] strings, String delimiter) {
+		int length = strings.length;
+		if (length == 0) return "";
+		StringBuilder words = new StringBuilder(strings[0]);
+		for (int i = 1; i < length; i++) {
+			words.append(delimiter).append(strings[i]);
+		}
+		return words.toString();
 	}
 }
